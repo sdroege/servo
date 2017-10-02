40760acd206eeed72ff0dcb7555135ff03722bb7
Sebastian Dröge <sebastian@centricular.com>
2017-10-02 19:44:43 +0200

    Fix compiler warning about unused parameter

--- components/script/dom/htmlmediaelement.rs
+++ components/script/dom/htmlmediaelement.rs
@@ -54,7 +54,7 @@ use js::jsapi::JSTracer;
 // TODO(philn): this doesn't belong here.
 #[allow(unsafe_code)]
 unsafe impl JSTraceable for RefCell<playground::player::Player> {
-    unsafe fn trace(&self, trc: *mut JSTracer) {
+    unsafe fn trace(&self, _trc: *mut JSTracer) {
     }
 }
 
